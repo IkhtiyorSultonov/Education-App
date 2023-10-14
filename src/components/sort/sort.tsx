d647bac0afd63f247153b1849d18f5203d24006d
sultonov ixtiyor <ixtiyor3953@gmail.com>
2023-10-14 20:38:48 +0500

    Sort Component

--- src/components/sort/sort.tsx
+++ src/components/sort/sort.tsx
@@ -4,19 +4,19 @@ import SortIco from"./sort.svg"
 import cn from 'classnames'
 import styles from"./sort.module.css"
 
-const sort = ({setSort,sort=SortEnum.Price,className,...props}:Sortprops):JSX.Element => {
+const sort = ({setSort,sort,className,...props}:Sortprops):JSX.Element => {
   return (
     <div className={cn(className,styles.sort)} {...props}>
         <span className={cn({
             [styles.active]:sort==SortEnum.Rating,
 
-        })}>
+        })} onClick={()=>setSort(SortEnum.Rating)}>
             <SortIco className={styles.SortIco}/>Rating
         </span>
         <span className={cn({
             [styles.active]:sort==SortEnum.Price
             
-        })}>
+        })}onClick={()=>setSort(SortEnum.Price)}>
             <SortIco className={styles.SortIco}/>Price
         </span>
     </div>
--- src/page-components/course-page-components/course-page-components.tsx
+++ src/page-components/course-page-components/course-page-components.tsx
@@ -1,22 +1,30 @@
 import { CoursePageComponentProps } from "./course-page-components.props";
 import styles from "./course-page-components.module.css";
 import { Advantages, Heading, HhData, Products, Sort, Tag, Text } from "@/components";
+import { SortReducer } from "./course.reducer";
+import { SortEnum } from "@/components/sort/sort.props";
+import { useReducer } from "react";
 
 const coursePageComponents = ({
   firstCategory,
   page,
   products,
 }: CoursePageComponentProps): JSX.Element => {
+  const [state, dispatch] = useReducer(SortReducer,{sort:SortEnum.Rating,products:products})
+ const setSort=(sort:SortEnum)=>{
+    dispatch({type:sort})
+ }  
 
+  
   return (
     <div className={styles.wrapper}>
       {/*TITLE*/}
       <div className={styles.title}>
         <Heading tag="h1">{page.title}</Heading>
-       <Sort />
+       <Sort  sort={state.sort} setSort={setSort}/>
       </div>
       {/*PRODUCTS*/}
-      <div>{products && products.map((c, idx) => <Products key={idx} product={c} />)}</div>
+      <div>{state.products && state.products.map((c, idx) => <Products key={idx} product={c} />)}</div>
       {/*VACATIONS*/}
       <div className={styles.hhTitle}>
         <Heading tag="h2">Vacations- {page.category}</Heading>
